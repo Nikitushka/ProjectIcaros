285be27285c3c4e9cb6c8347c65391f35e61f10a
Nikitushka <nikita-ponomarev@outlook.com>
2021-05-10 21:01:03 +0300

    cleaned code and added comments

--- Code/openstreetmap_python/app.py
+++ Code/openstreetmap_python/app.py
@@ -5,38 +5,36 @@ import json
 from parsers import csv_parse, wifi_parse
 import os
 
+# initialize the app and DB
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test@localhost/icaros'
-app.config.from_pyfile('config.py')
 db = sqlA(app)
 db.create_all()
 
-# map
+# map for index
 @app.route("/")
 def map():
     return render_template('index.html')
 
-# return EVERYTHING
+# neatly return all data in a json object
 @app.route("/api")
 def api():
- 
-    # trying out a pure sql solution
-
     radioresults = db.engine.execute("SELECT * FROM scan WHERE ssid IS NULL").fetchall()
     wifiresults = db.engine.execute("SELECT * FROM scan WHERE ssid IS NOT NULL").fetchall()
 
     return {"total": (len(radioresults) + len(wifiresults)), "sdr": [dict(row) for row in radioresults], "wifi": [dict(row) for row in wifiresults]}
     
 # return wifi data only or post wifi data
-@app.route("/api/wifi", methods = ['GET', 'POST'])
+@app.route("/api/wifi", methods = ['GET', 'POST']) # allowed HTTP methods
 def wifi():
     if request.method == 'POST':
         
         data = request.get_json()
-        # return data
+        # call nmcli on system and write a quick scan to the specified file location (make this definable by an .env file at some point?)
         os.system("nmcli -t -m multiline dev wifi list ifname wlan1 > output/wifi_out")
-        result = wifi_parse(data["lat"], data["lon"])
+        result = wifi_parse(data["lat"], data["lon"]) # parse the output file with the coords, returning a list of objects
 
+        # prepare an insert statement and insert the parsed output an object at a time
         statement = """INSERT INTO scan(lat, lon, ssid, encryption) VALUES(%(lat)s, %(lon)s, %(SSID)s, %(SECURITY)s)"""
         for line in result:
             db.engine.execute(statement, line)
@@ -44,16 +42,18 @@ def wifi():
         
     else:
         
+        # if the HTTP method is GET, return only the wifi data
         wifiresults = db.engine.execute("SELECT * FROM scan WHERE ssid IS NOT NULL").fetchall()
         return {"total": len(wifiresults), "wifi": [dict(row) for row in wifiresults]}
 
-# return sdr data only or post sdr data
+# return sdr data only or post sdr data, the gist of it is the same as in the /wifi endpoint
 @app.route("/api/sdr", methods = ['GET', 'POST'])
 def radio():
     if request.method == 'POST':
         
         data = request.get_json()
-        os.system("rtl_power -1 -f 87M:107M:200k output/sdr.csv")
+        # call rtl_power on the system for 1 second and write the output to the specified file location
+        os.system("rtl_power -1 -f 87M:107M:200k output/sdr.csv") 
         result = csv_parse(data["lat"], data["lon"])
         
         statement = """INSERT INTO scan(lat, lon, sdr_frequency, signal_strength) VALUES(%(lat)s, %(lon)s, %(sdr_frequency)s, %(signal_strength)s)"""
@@ -61,15 +61,10 @@ def radio():
             db.engine.execute(statement, line)
         return json.dumps(result)
         
-
-
     else:
         
         radioresults = db.engine.execute("SELECT * FROM scan WHERE ssid IS NULL").fetchall()
         return {"total": len(radioresults), "sdr": [dict(row) for row in radioresults]}
-
-def insert_wifi(frequency, lat, lon, modulation, strength):
-    print("placeholder")
 
 if __name__ == "__main__":
     app.run(debug=True)
